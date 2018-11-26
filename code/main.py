26576054f6352560f2c5446f44761e00ae5b4571
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-11-26 20:56:17 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,3 +1,46 @@
 
 
 
+def parse_file(file_uri):
+    # dark knight rises ; url223 ;
+
+    
+
+    pass
+
+
+
+
+def add_index(context, url):
+
+
+
+    pass
+
+
+
+
+def make_search(word):
+
+
+
+    # list of {context,url,point}
+    pass
+
+
+
+
+if __name__ == "__main__":
+
+    # while true
+    # add_index
+
+
+
+
+
+    pass
+
+
+
+
