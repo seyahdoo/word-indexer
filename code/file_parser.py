72ca5322dc3f94098dd048fe04ed3e2128972ed4
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-11-27 15:06:33 +0300

    added a requirement code

--- code/file_parser.py
+++ code/file_parser.py
@@ -8,6 +8,10 @@ def parse_file(file_path):
     # dark knight rises ; url223 ;
     print("parsing file")
 
+    if len(file_path) <= 0:
+        print("input file path length has to be longer than 0, returning without processing")
+        return
+
     size = os.path.getsize(file_path)
     if size > 20000000:
         print("input file size is too large, returning without processing")
